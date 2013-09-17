327dcbb1e2c7a9612ea40bbae3daf12d9b3519cf
Adriano Resende <zullcore@gmail.com>
2013-09-17 17:08:48 -0300

    Arruma o formulário

--- core/forms.py
+++ core/forms.py
@@ -3,15 +3,38 @@ from django import forms
 
 from contact_form.forms import ContactForm
 
+"""
+  Campo de select para 'Assunto'
+"""
+subjects = (
+    (u'0', u'Experimentar o sistema'),
+    (u'1', u'Fazer plano'),
+    (u'3', u'Dúvida'),
+    (u'4', u'Outros'),
+  )
+
+"""
+  Como de select para 'Como nos encontrou?'
+"""
+founds = (
+    (u'0', u'Sites de buscas (Google, Blings, etc.)'),
+    (u'1', u'Indicação'),
+    (u'3', u'E-mail'),
+    (u'4', u'Outros'),
+  )
 
 class EventsContactForm(ContactForm):
     name = forms.CharField(max_length=100,
                            label=u'Nome')
     email = forms.EmailField(max_length=200,
                              label=u'E-mail')
-    body = forms.CharField(widget=forms.Textarea(attrs={'rows': '15'}),
+    body = forms.CharField(widget=forms.Textarea(attrs={'rows': '11'}),
                            label=u'Mensagem')
     company = forms.CharField(max_length=100,
                               label=u'Empresa')
     telephone = forms.CharField(max_length=100,
                                 label=u'Telefone')
+    found = forms.ChoiceField(label=u'Como nos encontrou?',
+                              choices=founds)
+    subject = forms.ChoiceField(label=u'Assunto',
+                                choices=subjects)
